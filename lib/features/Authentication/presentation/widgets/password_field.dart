3f9f01e24a438b1ee49ba9a4b3f9484dbb52c246
markcabalona <markcabalona58@gmail.com>
2023-02-18 14:06:57 +0800

    Password Visibility     - added password Visibility controller

--- lib/features/Authentication/presentation/widgets/password_field.dart
+++ lib/features/Authentication/presentation/widgets/password_field.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:foobar_shop/core/utils/validators/password.dart';
 
-class PasswordField extends StatelessWidget {
+class PasswordField extends StatefulWidget {
   const PasswordField({
     Key? key,
     this.initialValue,
@@ -17,24 +18,63 @@ class PasswordField extends StatelessWidget {
   final String? Function(String? value)? validator;
 
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController controller = TextEditingController(
-      text: initialValue,
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  late final TextEditingController controller;
+  bool isHidden = false;
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController(
+      text: widget.initialValue,
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      onChanged: onChanged,
+      onChanged: widget.onChanged,
       validator: (value) {
-        if (validator != null) {
-          return validator!(value);
+        if (widget.validator != null) {
+          return widget.validator!(value);
         }
         return passwordValidator(value);
       },
       autovalidateMode: AutovalidateMode.onUserInteraction,
       keyboardType: TextInputType.visiblePassword,
-      textInputAction: textInputAction,
-      decoration: const InputDecoration(
-          hintText: 'Password', label: Text('Password'), errorMaxLines: 4),
+      textInputAction: widget.textInputAction,
+      obscureText: isHidden,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        label: const Text('Password'),
+        errorMaxLines: 4,
+        suffix: GestureDetector(
+          onTap: () {
+            setState(() {
+              isHidden = !isHidden;
+            });
+          },
+          child: AnimatedSwitcher(
+            duration: 300.ms,
+            transitionBuilder: (child, animation) => ScaleTransition(
+              scale: animation,
+              child: child,
+            ),
+            child: isHidden
+                ? const Icon(
+                    key: ValueKey(false),
+                    Icons.visibility_off,
+                  )
+                : const Icon(
+                    key: ValueKey(true),
+                    Icons.visibility,
+                  ),
+          ),
+        ),
+      ),
     );
   }
 }
